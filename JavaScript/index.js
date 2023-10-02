903d44596bf2915f02c9934e8e59332734d7ae30
ericalundstrom <erica.lundstrom99@hotmail.com>
2023-10-02 13:28:02 +0200

    small changes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,6 @@
 function RenderBoilPage() {
-  basicLayout();
-  wrapper.innerHTML += `
+    basicLayout();
+    wrapper.innerHTML += `
     <div id="box">
         <div class="optionDiv">
             <p> How would you like your egg? </p>
@@ -20,62 +20,66 @@ function RenderBoilPage() {
     <button id="startTimer"> Start boiling </button>
   `;
 
-  wrapper.querySelector("#eggPic").src = "/images/eggOutline.png";
-  wrapper.querySelector("#title").src = "/images/NewTitle.png";
+    wrapper.querySelector("#eggPic").src = "/images/eggOutline.png";
+    wrapper.querySelector("#title").src = "/images/NewTitle.png";
 
-  let chosenOpt = {};
+    let chosenOpt = {};
 
-  let option = wrapper.querySelectorAll(".optionDiv");
+    let option = wrapper.querySelectorAll(".optionDiv");
 
-  option.forEach((opt) => {
-    opt.addEventListener("click", (event) => {
-      event.target.classList.toggle("chosen");
+    option.forEach((opt) => {
+        opt.addEventListener("click", (event) => {
+            if (event.target.classList == "optionDiv" || event.target.localName === "p") {
+                console.log("fel");
+            } else {
+                event.target.classList.toggle("chosen");
+            }
 
-      let parent = event.target.parentElement;
-      let childs = parent.querySelectorAll("div");
+            let parent = event.target.parentElement;
+            let childs = parent.querySelectorAll("div");
 
-      childs.forEach((child) => {
-        if (child.classList == "option chosen") {
-          if (
-            child.textContent === "Soft" ||
-            child.textContent === "Half" ||
-            child.textContent === "Hard"
-          ) {
-            chosenOpt.liking = child.textContent;
-          }
+            childs.forEach((child) => {
+                if (child.classList == "option chosen") {
+                    if (
+                        child.textContent === "Soft" ||
+                        child.textContent === "Half" ||
+                        child.textContent === "Hard"
+                    ) {
+                        chosenOpt.liking = child.textContent;
+                    }
 
-          if (
-            child.textContent === "Small" ||
-            child.textContent === "Medium" ||
-            child.textContent === "Large"
-          ) {
-            chosenOpt.size = child.textContent;
-          }
-        }
-      });
+                    if (
+                        child.textContent === "Small" ||
+                        child.textContent === "Medium" ||
+                        child.textContent === "Large"
+                    ) {
+                        chosenOpt.size = child.textContent;
+                    }
+                }
+            });
+        });
     });
-  });
 
-  wrapper.querySelector("#startTimer").addEventListener("click", () => {
-    if (chosenOpt.liking && chosenOpt.size && chosenOpt.temp !== "") {
-      let chosen = document.querySelectorAll(".chosen");
-      if (chosen.length !== 3) {
-        popUp("You can only chose one of each option!");
-      }
-      StartTimer(chosenOpt);
-    } else {
-      popUp("You can only chose one of each option!");
-    }
-  });
+    wrapper.querySelector("#startTimer").addEventListener("click", () => {
+        if (chosenOpt.liking && chosenOpt.size && chosenOpt.temp !== "") {
+            let chosen = document.querySelectorAll(".chosen");
+            if (chosen.length !== 3) {
+                popUp("You can only chose one of each option!");
+            }
+            StartTimer(chosenOpt);
+        } else {
+            popUp("You can only chose one of each option!");
+        }
+    });
 }
 
 function StartTimer(opt) {
-  basicLayout();
+    basicLayout();
 
-  let divDom;
-  divDom = `${opt.liking}_${opt.size}`;
+    let divDom;
+    divDom = `${opt.liking}_${opt.size}`;
 
-  wrapper.innerHTML += `
+    wrapper.innerHTML += `
         <div id="timer">
             <p id="promp">Fun facts about eggs:</p>
             <p id="funFact"></p>
@@ -83,23 +87,20 @@ function StartTimer(opt) {
             <div id="clock"> 00:00 </div>
         </div>
     `;
-  wrapper.querySelector("#eggIcon").classList.add("wiggle");
+    wrapper.querySelector("#eggIcon").classList.add("wiggle");
 
-  wrapper.querySelector("#eggPic").src = "/images/eggOutline.png";
-  wrapper.querySelector("#title").src = "/images/NewTitle.png";
+    wrapper.querySelector("#eggPic").src = "/images/eggOutline.png";
+    wrapper.querySelector("#title").src = "/images/NewTitle.png";
 
-  wrapper.querySelector("#eggIcon").style.position = "relative";
-  wrapper.querySelector("#eggIcon").style.bottom = "35px";
+    wrapper.querySelector("#eggIcon").style.position = "relative";
+    wrapper.querySelector("#eggIcon").style.bottom = "35px";
 
-  // document.querySelector("#funFact").textContent = "Fun fact";
-  timer_function(divDom);
+    timer_function(divDom);
 
-  displayNextFact(op);
-  document.querySelector("#funFact").textContent = "Fun fact";
-  timer_function(divDom);
+    displayNextFact(op);
+    document.querySelector("#funFact").textContent = "Fun fact";
+    timer_function(divDom);
 
-  //   const funFact = wrapper.querySelector("#funFact");
-  //   displayNextFact(funFact, true);
 }
 
 let op = true;
@@ -107,128 +108,139 @@ let op = true;
 let i = 0;
 
 function displayNextFact(opt) {
-  const funFact = wrapper.querySelector("#funFact");
+    const funFact = wrapper.querySelector("#funFact");
 
-  if (opt !== false) {
-    if (i < eggInfo.length) {
-      // Apply a fade-out effect
-      funFact.style.opacity = 0;
+    if (opt) {
+        if (i < eggInfo.length) {
+            // Apply a fade-out effect
+            funFact.style.opacity = 0;
 
-      // Wait for the fade-out transition to complete
-      setTimeout(() => {
-        funFact.textContent = eggInfo[i];
+            // Wait for the fade-out transition to complete
+            setTimeout(() => {
+                funFact.textContent = eggInfo[i];
 
-        // Apply a fade-in effect
-        funFact.style.opacity = 1;
-        funFact.style.transitionProperty = "opacity";
-        funFact.style.transitionDuration = "2s";
+                // Apply a fade-in effect
+                funFact.style.opacity = 1;
+                funFact.style.transitionProperty = "opacity";
+                funFact.style.transitionDuration = "2s";
 
-        i++;
-        setTimeout(displayNextFact, 3000); // Change the delay time (in milliseconds) as needed
-      }, 1000); // This timeout should match the transition duration
-    } else {
-      i = 0;
-      displayNextFact();
+                i++;
+                setTimeout(displayNextFact, 3000); // Change the delay time (in milliseconds) as needed
+            }, 1000); // This timeout should match the transition duration
+        } else {
+            i = 0;
+            displayNextFact();
+        }
     }
-  }
 }
 
 function timer_function(option) {
-  // Clear any previous timers
+    // Clear any previous timers
 
-  const timerDisplay = document.getElementById("clock");
-  let countdown; // Variable to store the timer ID
-  let secondsRemaining; // Variable to store the remaining seconds
+    const timerDisplay = document.getElementById("clock");
+    let countdown; // Variable to store the timer ID
+    let secondsRemaining; // Variable to store the remaining seconds
 
-  // Define the timer durations for different options
-  const timerDurations = {
-    Soft_Small: 6 * 60, // 6 minutes
-    Soft_Medium: 6 * 60 + 43, // 6 minutes 43 seconds
-    Soft_Large: 7 * 60 + 28, // 7 minutes 28 seconds
+    // Define the timer durations for different options
+    const timerDurations = {
+        // Soft_Small: 6 * 60, // 6 minutes
+        Soft_Small: 5, // 6 minutes
+        Soft_Medium: 6 * 60 + 43, // 6 minutes 43 seconds
+        Soft_Large: 7 * 60 + 28, // 7 minutes 28 seconds
 
-    Half_Small: 6 * 60 + 49, // 6 minutes 49 seconds
-    Half_Medium: 7 * 60 + 43, // 7 minutes 43 seconds
-    Half_Large: 8 * 60 + 35, // 8 minutes 35 seconds
+        Half_Small: 6 * 60 + 49, // 6 minutes 49 seconds
+        Half_Medium: 7 * 60 + 43, // 7 minutes 43 seconds
+        Half_Large: 8 * 60 + 35, // 8 minutes 35 seconds
 
-    Hard_Small: 8 * 60 + 45, // 8 minutes 45 seconds
-    // Hard_Small: 45,
-    Hard_Medium: 10 * 60, // 10 minutes
-    Hard_Large: 11 * 60, // 11 minutes
-  };
+        Hard_Small: 8 * 60 + 45, // 8 minutes 45 seconds
+        // Hard_Small: 45,
+        Hard_Medium: 10 * 60, // 10 minutes
+        Hard_Large: 11 * 60, // 11 minutes
+    };
 
-  // Get the timer duration based on the selected option
-  clearTimeout(countdown);
-  const duration = timerDurations[option];
+    // Get the timer duration based on the selected option
+    clearTimeout(countdown);
+    const duration = timerDurations[option];
 
-  secondsRemaining = duration;
+    secondsRemaining = duration;
 
-  // Function to update the timer display
-  function updateTimer() {
-    if (secondsRemaining <= 0) {
-      // If time is up, display "Go get your egg!"
-      // timerDisplay.textContent = "Go get your egg!";
-      popUp("The time is up!");
-      wrapper.innerHTML += `
-                <button onclick="renderStartPage()"> Boil another </button>
-        `;
-    } else {
-      // Calculate and display the remaining minutes and seconds
-      const minutes = Math.floor(secondsRemaining / 60);
-      const seconds = secondsRemaining % 60;
+    // Function to update the timer display
+    function updateTimer() {
+        if (secondsRemaining <= 0) {
+            // If time is up, display "Go get your egg!"
+            // timerDisplay.textContent = "Go get your egg!";
+            popUp("The time is up!", "fun");
+            wrapper.querySelector("#eggIcon").classList.remove("wiggle");
+            let op = false;
+            displayNextFact(op)
+            wrapper.querySelector("#promp").textContent = "";
+            // removeBubbles();
+        } else {
+            // Calculate and display the remaining minutes and seconds
+            const minutes = Math.floor(secondsRemaining / 60);
+            const seconds = secondsRemaining % 60;
 
-      timerDisplay.textContent = `${minutes}:${
-        seconds < 10 ? "0" : ""
-      }${seconds}`;
+            timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" : ""
+                }${seconds} `;
 
-      // Check if the option does not include "Soft"
-      const isSoftBoiled = option.includes("Soft");
+            // Check if the option does not include "Soft"
+            const isSoftBoiled = option.includes("Soft");
 
-      // Calculate the percentage of time remaining
-      const percentageRemaining = (secondsRemaining / duration) * 100;
+            // Calculate the percentage of time remaining
+            const percentageRemaining = (secondsRemaining / duration) * 100;
 
-      // Update yolk color based on percentageRemaining and option
-      const yolk = document.getElementById("yolk");
-      if (!isSoftBoiled && percentageRemaining < 25) {
-        // When there's less than 10% of time remaining and not "Soft," make yolk more orange
-        yolk.style.backgroundColor = "orange"; // Orange
-      } else {
-        // Otherwise, keep it yellow
-        yolk.style.backgroundColor = "yellow"; // Yellow
-      }
+            // Update yolk color based on percentageRemaining and option
+            const yolk = document.getElementById("yolk");
+            if (!isSoftBoiled && percentageRemaining < 25) {
+                // When there's less than 10% of time remaining and not "Soft," make yolk more orange
+                yolk.style.backgroundColor = "orange"; // Orange
+            } else {
+                // Otherwise, keep it yellow
+                yolk.style.backgroundColor = "yellow"; // Yellow
+            }
 
-      // Decrement the remaining seconds
-      secondsRemaining--;
+            // Decrement the remaining seconds
+            secondsRemaining--;
 
-      // Schedule the next update in 1 second
-      countdown = setTimeout(updateTimer, 1000);
+            // Schedule the next update in 1 second
+            countdown = setTimeout(updateTimer, 1000);
+            let done = true
+            animateBubbles(done);
+        }
     }
-  }
-  secondsRemaining = duration;
-  animateBubbles();
-  updateTimer();
+    secondsRemaining = duration;
+    updateTimer();
 }
 
 function createBubble() {
-  // Create the bubbles container
-  const bubblesContainer = document.createElement("div");
-  bubblesContainer.id = "bubbles"; // Set the ID for styling
 
-  // Append the bubbles container to the wrapper
-  wrapper.appendChild(bubblesContainer);
+    // Create the bubbles container
+    const bubblesContainer = document.createElement("div");
+    bubblesContainer.id = "bubbles"; // Set the ID for styling
 
-  // Create the bubble div
-  const bubble = document.createElement("div");
-  bubble.className = "bubble";
-  bubble.style.left = `${Math.random() * 100}%`;
-  bubble.style.animationDuration = `${Math.random() * 7 + 2}s`; // Random duration between 2 to 7 seconds
-  bubblesContainer.appendChild(bubble);
+    // Append the bubbles container to the wrapper
+    wrapper.appendChild(bubblesContainer);
 
-  // Remove the bubble element once it reaches the top
-  bubble.addEventListener("animationiteration", () => {
-    bubble.remove();
-  });
+    // Create the bubble div
+    const bubble = document.createElement("div");
+    bubble.className = "bubble";
+    bubble.style.left = `${Math.random() * 100}% `;
+    bubble.style.animationDuration = `${Math.random() * 7 + 2} s`; // Random duration between 2 to 7 seconds
+    bubblesContainer.appendChild(bubble);
+    // Remove the bubble element once it reaches the top
+    bubble.addEventListener("animationiteration", () => {
+        bubble.remove();
+    });
 }
 
-function animateBubbles() {
-  setInterval(createBubble, 1000); // Create a new bubble every 1 second
+function animateBubbles(done) {
+    setInterval(createBubble, 1000); // Create a new bubble every 1 second
+}
+
+function removeBubbles() {
+    let bubbles = document.querySelectorAll(".bubble");
+    console.log(bubbles);
+    bubbles.forEach(bubble => {
+        delete bubble;
+    })
 }
